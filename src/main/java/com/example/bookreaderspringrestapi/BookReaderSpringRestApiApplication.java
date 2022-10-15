9632e2c79a5e1f37042f42884b42aa4bc209ce56
beatrizgomes <beatrizgomesxx@gmail.com>
2022-10-15 17:40:20 -0300

    Initial commit

---
+++ src/main/java/com/example/bookreaderspringrestapi/BookReaderSpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookreaderspringrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReaderSpringRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookReaderSpringRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookreaderspringrestapi/BookReaderSpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookreaderspringrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReaderSpringRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
