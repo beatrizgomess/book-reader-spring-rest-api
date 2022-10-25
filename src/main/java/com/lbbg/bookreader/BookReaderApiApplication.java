73b771769f58af6c4b4aa6a878c24cfdcb750d7b
beatrizgomes <beatrizgomesxx@gmail.com>
2022-10-25 11:23:02 -0300

    add class models and repositories

--- src/main/java/com/lbbg/bookreader/BookReaderApiApplication.java
+++ src/main/java/com/lbbg/bookreader/BookReaderApiApplication.java
@@ -1,13 +1,13 @@
-package com.example.bookreaderspringrestapi;
+package com.lbbg.bookreader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookReaderSpringRestApiApplication {
+public class BookReaderApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BookReaderSpringRestApiApplication.class, args);
+        SpringApplication.run(BookReaderApiApplication.class, args);
     }
 
 }
---
+++ src/main/java/com/lbbg/bookreader/entity/User.java
@@ -0,0 +1,29 @@
+package com.lbbg.bookreader.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.swing.*;
+import java.util.Date;
+import java.util.List;
+
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String surname;
+    private Date birth_date;
+    private Sex sex;
+    private Address address;
+    private ImageIcon image;
+    private List<Book> books;
+    private Role role;
+
+}
